4a9d1703fee722c6ff2153d4e85f166a908381c7
Misael97001 <chiluisajefferson97@gmail.com>
2025-12-01 18:54:16 -0500

    clase autos

---
+++ Clases/src/com/krakedev/TestAuto.java
@@ -0,0 +1,29 @@
+package com.krakedev;
+
+public class TestAuto {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	 Auto auto1 =new Auto();
+	 Auto auto2=new Auto();
+	 
+	 auto1.marca="Mercedes";
+	 auto1.anio=2025;
+	 auto1.precio=(float) 312.22;
+	 
+	 System.out.println("Marca: "+auto1.marca);
+	 System.out.println("Anio: "+auto1.anio);
+	 System.out.println("Precio: "+auto1.precio);
+		
+	 auto2.marca="Chevrolet";
+	 auto2.anio=2023;
+	 auto2.precio=(float) 432.22;
+	 
+	 System.out.println("Marca: "+auto2.marca);
+	 System.out.println("Anio: "+auto2.anio);
+	 System.out.println("Precio: "+auto2.precio);
+	 
+	}
+
+}
