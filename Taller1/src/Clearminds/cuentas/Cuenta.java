1e08a1c1e306312515569ae9934609956b398937
Misael97001 <chiluisajefferson97@gmail.com>
2025-12-02 20:48:27 -0500

    Taller 1

---
+++ Taller1/src/Clearminds/cuentas/Cuenta.java
@@ -0,0 +1,64 @@
+package Clearminds.cuentas;
+
+public class Cuenta {
+
+	private String id;
+	private String tipo;
+	private double saldo;
+	
+	public Cuenta(String id) {
+		this.id=id;
+		this.tipo="A";
+	}
+	public Cuenta(String id, String tipo, double saldo) {
+		this.id=id;
+		this.tipo=tipo;
+		this.saldo=saldo;
+	}
+	
+	public void imprimir() {
+		System.out.println("************");
+		System.out.println("CUENTA");
+		System.out.println("************");
+		System.out.println("Nombre de Cuenta: "+id);
+		System.out.println("Tipo: "+tipo);
+		System.out.println("Saldo: "+saldo);
+
+
+	}
+	
+	public void imprimirConMiEstilo() {
+		System.out.println();
+		
+		System.out.println("************");
+		System.out.println("CUENTA MI ESTILO");
+		System.out.println("************");
+		System.out.println("Nombre de Cuenta :) : "+id);
+		System.out.println("Tipo -_- : "+tipo);
+		System.out.println("Saldo $$$$: "+saldo);
+
+	}
+	
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	public double getSaldo() {
+		return saldo;
+	}
+	
+	public void setSaldo(double saldo) {
+		this.saldo=saldo;
+	}
+	
+	
+}
